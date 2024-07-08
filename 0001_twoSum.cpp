c95a5206ca2c541f22b063c585ff4e9affce236e
zainf2327 <zainchoudery786@gmail.com>
2024-07-08 19:41:46 +0500

    Two Pass HashMap Method Time Complexity: O(n) Space Complexity:O(n)

--- 0001_twoSum.cpp
+++ 0001_twoSum.cpp
@@ -1,32 +1,28 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<unordered_map>
 using namespace std;
 
 class Solution {
 public:
-    vector<int> twoSum(const vector<int>& nums, int target) {
-        vector<pair<int,int>> num_indices; // Vector pair holding value of array as first value and index of value of array as second value in vector array.
-         int size =nums.size();
-         for(int i= 0;i<size;i++) {
-            num_indices.push_back({nums[i],i});
-         }
+ vector<int> twoSum(const vector<int>& nums, int target) {
+        unordered_map<int, int> arrMap;
+        int n = nums.size();
 
-         sort(num_indices.begin(),num_indices.end()); 
-          // sort the vector pairs based on first value keeping the second value intact to original number and if first values are same then sorts according to second value.
-         int start=0, end = size-1,sum=0;
-         while(start<end)   {
-            sum=num_indices[start].first+num_indices[end].first;
-            if(sum==target)
-                return { num_indices[start].second , num_indices[end].second };
-            else if(sum>target)
-                end--;
-            else
-               start++;
-         }
+        for (int i = 0; i < n; i++) {
+            arrMap[nums[i]] = i;
+        }
 
-         // Returns empty vector in case of no pair of numbers are found that add to target.
-         return {};
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (arrMap.count(complement) && arrMap[complement] != i) {
+                return {i, arrMap[complement]};
+            }
+        }
+
+        return {}; //Return empty vector in case no pair of numbers are found that adds to target.
     }
 };
+
 
