896d2cbbf9a41e2b4be30dc2a6a36f1d9fdf7f4f
zainf2327 <zainchoudery786@gmail.com>
2024-07-11 15:24:22 +0500

    Two pass HashMap Method Time Complexity: O(n) Space Complexity: O(n)

--- 0167_twoSum.cpp
+++ 0167_twoSum.cpp
@@ -1,18 +1,22 @@
 #include<iostream>
 #include<vector>
+#include<unordered_map>
 using namespace std;
 class Solution {
 public:
-    vector<int> twoSum(const vector<int>& nums, int target)   {
-        for(int i=0;i<nums.size()-1;i++)    {
-             for(int j=i+1;j<nums.size();j++)   {
-                if(nums[j] == target - nums[i])
-                    return {i+1,j+1}; // 1-based index array
-
-             }
+    vector<int> twoSum(const vector<int>& numbers, int target) {
+        int size = numbers.size(),complement;
+        unordered_map<int, int> arrMap; // value,index
+        for (int i=0;i<size;i++)  {
+            arrMap[numbers[i]]=i;
         }
-        // Return empty vector in case of no pair of numbers found that add to target.
-        return {};
+
+        for (int i=0;i<size;i++)  {
+            complement = target-numbers[i];
+            if(arrMap.count(complement) && arrMap[complement]!=i)
+                    return { i+1,arrMap[complement]+1}; //1-based index array
+        }
+        
+        return {};  //Return empty array if no pair is found that add to target
     }
 };
-    
