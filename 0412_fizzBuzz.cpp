fcf6ba7e59d7efe4754bf85bb1392690f26e4c3a
zainf2327 <zainchoudery786@gmail.com>
2024-07-14 18:23:11 +0500

    Smarter Solution Time Complexity: O(n) Space Complexity : O(1)

--- 0412_fizzBuzz.cpp
+++ 0412_fizzBuzz.cpp
@@ -5,17 +5,17 @@ using namespace std;
 
 class Solution {
 public:
-      vector<string> fizzBuzz(int n) {
-            vector<string> result(n);
-            for(int i = 1; i <= n; i++)   {
-                if(i%3==0 && i%5==0)
-                    result[i-1]="FizzBuzz";
-                else if(i%3==0)
-                    result[i-1]="Fizz";
-                else if(i%5==0)
-                    result[i-1]="Buzz";
+      vector <string> fizzBuzz(int n) {
+            vector <string> result;
+            for (int i = 1; i <= n; i++)   {
+                if (i % 15 ==0)
+                   result.push_back("FizzBuzz");
+                else if (i % 3 ==0)
+                    result.push_back("Fizz");
+                else if (i % 5 ==0)
+                    result.push_back("Buzz");
                 else 
-                    result[i-1]=to_string(i);
+                    result.push_back(to_string(i));
             }
 
             return result;
