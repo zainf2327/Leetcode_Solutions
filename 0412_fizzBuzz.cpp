7d0fccdb792b25db03cd1b4a283cdfc65c76e4dd
zainf2327 <zainchoudery786@gmail.com>
2024-07-14 18:38:36 +0500

    Tricky Approach Time Complexity: O(n) Space Complexity; O(n)

--- 0412_fizzBuzz.cpp
+++ 0412_fizzBuzz.cpp
@@ -8,16 +8,14 @@ public:
       vector <string> fizzBuzz(int n) {
             vector <string> result;
             for (int i = 1; i <= n; i++)   {
-                if (i % 15 ==0)
-                   result.push_back("FizzBuzz");
-                else if (i % 3 ==0)
-                    result.push_back("Fizz");
-                else if (i % 5 ==0)
-                    result.push_back("Buzz");
-                else 
-                    result.push_back(to_string(i));
+                result.push_back (
+                    (i % 15 == 0) ? "FizzBuzz" :
+                    (i % 5 == 0)  ? "Buzz" :
+                    (i % 3 == 0)  ? "Fizz" :
+                    to_string(i)
+                );
+                
             }
-
             return result;
         }
 };
