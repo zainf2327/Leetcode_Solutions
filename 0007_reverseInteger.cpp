5228bf652fcdc18b5671bcde5c6ec00c921b4585
zainf2327 <zainchoudery786@gmail.com>
2024-07-17 17:19:07 +0500

    Reverse X where X is long long int Time Complexity: O(n) Space Complexity: O(1)

--- 0007_reverseInteger.cpp
+++ 0007_reverseInteger.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 class Solution {
 public:
-    int reverse(int n) {
+    int reverse(long long int n) {
         int rev = 0;
-        int Max = INT_MAX / 10, Min = INT_MIN / 10;
+        int Max = INT_MAX / 10, Min = INT_MIN / 10, rem;
         while (n) {
-
-            if (rev > Max || rev < Min)  return 0;
-            rev = (rev * 10) + (n % 10);
+            rem = n % 10;
+            if (rev > Max || rev == Max && rem > (INT_MAX % 10))
+                return 0;
+            if (rev < Min || rev == Min && rem < (INT_MIN % 10))
+                return 0;
+            rev = (rev * 10) + rem;
             n /= 10;
         }
         return rev;
