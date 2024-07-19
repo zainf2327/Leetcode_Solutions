dd516f91a723135d1d7438a7d1bd9bfa1dfad13a
zainf2327 <zainchoudery786@gmail.com>
2024-07-20 00:59:06 +0500

    HashMap Method Time Complexity: O(n2) Space Complexity; O(n)

--- 0015_threeSum.cpp
+++ 0015_threeSum.cpp
@@ -1,27 +1,44 @@
 #include <iostream>
 #include <vector>
 #include<algorithm>
-#include <set>
+#include <unordered_map>
 using namespace std;
 class Solution {
 public:
-    vector<vector<int>> threeSum(const vector<int>& nums) {          
-        set<vector<int>> temp;
-        sort(nums.begin(),nums.end());                      // sort the array so when we get desired triplets, they are in sorted order,so as they would be added into set,they would be removed.
-        int n =  nums.size();
-        for(int i=0;i<n-2;i++)    {
-            if(nums[i]>0) break;  
-            for(int j=i+1;j<n-1;j++)   {
-                for(int k=j+1;k<n;k++)    {
-                    if(nums[i]+nums[j]+nums[k]==0) {                // target in this case is 0. It can be any value
-                         
-                        temp.insert({nums[i],nums[j],nums[k]});            // insert the triple in map so that duplicates are removed
-                    }
-                }   
-            }        
+    vector<vector<int>> threeSum(vector<int>& nums)    {          
+        int n = nums.size();        // size of array
+        
+        if (n < 3)  return {};    // Base Case 1
+
+        sort(nums.begin(), nums.end());  // Sorting of array
+
+        if (nums[0] > 0)    return {};   // Base Case 2
+        
+        unordered_map<int, int> hashMap;
+        for (int i = 0; i < n; i++)    {
+            hashMap[nums[i]] = i;        // Hashing of indices
         }
-        vector<vector<int>> ans (temp.begin(),temp.end()); // copy the elements of map(triples) into vector 
-        return ans; // return the vector containing unique triples that add to zero.
+
+        vector<vector<int>> triples;
+        
+        for (int i = 0; i < n - 2; i++)    {   //Fixing 1st number to tranverse the array
+
+            if (nums[i] > 0)    break;      // if fixed number is +ve, stop it since leading elements would ony increase the sum and we cannot acheive 0(target).(only if target==0)
+            
+            for (int j = i + 1; j < n - 1; j++)    {   //Fixing 2nd number
+
+                
+                int complement = -1 * (nums[i] + nums[j]);     //complement = target - nums[i]-nums[j]
+
+                if (hashMap.count(complement) && hashMap[complement] > j)
+                        triples.push_back({nums[i], nums[j], complement});      //If it exists in hashmap and its last occurrence index > 2nd fixed index, we found our triplet.
+
+                j=hashMap[nums[j]];                     //Update j to last occurence of 2nd fixed number to avoid duplicate triplets.
+            }
+
+            i=hashMap[nums[i]];                       //Update i to last occurence of 1st fixed number to avoid duplicate triplets.
+        }
+        return triples;
     }       
     
 };
