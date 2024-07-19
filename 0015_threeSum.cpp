16bfef4b0ffecbc985e9fbae4d77cf824a0d5dde
zainf2327 <zainchoudery786@gmail.com>
2024-07-19 22:03:09 +0500

    Better Brute Force Solution Time Complexity: O(n3) Space Complexity: O(n)

--- 0015_threeSum.cpp
+++ 0015_threeSum.cpp
@@ -10,7 +10,7 @@ public:
         sort(nums.begin(),nums.end());                      // sort the array so when we get desired triplets, they are in sorted order,so as they would be added into set,they would be removed.
         int n =  nums.size();
         for(int i=0;i<n-2;i++)    {
-            if(nums[i]>0) break;
+            if(nums[i]>0) break;  
             for(int j=i+1;j<n-1;j++)   {
                 for(int k=j+1;k<n;k++)    {
                     if(nums[i]+nums[j]+nums[k]==0) {                // target in this case is 0. It can be any value
