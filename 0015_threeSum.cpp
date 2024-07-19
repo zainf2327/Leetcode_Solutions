313ec420a30b3fa15d401c71f663d4a2aca571ec
zainf2327 <zainchoudery786@gmail.com>
2024-07-19 21:53:50 +0500

    Brute Force Solution Time Complexity: O(n3) Space Complexity: O(n)

---
+++ 0015_threeSum.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include<algorithm>
+#include <set>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> threeSum(const vector<int>& nums) {          
+        set<vector<int>> temp;
+        vector <int> res;
+        int n =  nums.size();
+        for(int i=0;i<n-2;i++)    {
+            for(int j=i+1;j<n-1;j++)   {
+                for(int k=j+1;k<n;k++)    {
+                    if(nums[i]+nums[j]+nums[k]==0) { // target in this case is 0. It can be any value
+                        res={nums[i],nums[j],nums[k]};
+                        sort(res.begin(),res.end()); // sort the triple
+                        temp.insert(res);            // insert the triple in map so that duplicates are removed
+                    }
+                }   
+            }        
+        }
+        vector<vector<int>> ans (temp.begin(),temp.end()); // copy the elements of map(triples) into vector 
+        return ans; // return the vector containing unique triples that add to zero.
+    }       
+    
+};
