f43daa22ce11a1bed3c8bca9282fc22dae562a75
zainf2327 <zainchoudery786@gmail.com>
2024-07-19 22:01:18 +0500

    Better Brute Force Solution Time Complexity: O(n3) Space Complexity: O(n)

--- 0015_threeSum.cpp
+++ 0015_threeSum.cpp
@@ -7,15 +7,15 @@ class Solution {
 public:
     vector<vector<int>> threeSum(const vector<int>& nums) {          
         set<vector<int>> temp;
-        vector <int> res;
+        sort(nums.begin(),nums.end());                      // sort the array so when we get desired triplets, they are in sorted order,so as they would be added into set,they would be removed.
         int n =  nums.size();
         for(int i=0;i<n-2;i++)    {
+            if(nums[i]>0) break;
             for(int j=i+1;j<n-1;j++)   {
                 for(int k=j+1;k<n;k++)    {
-                    if(nums[i]+nums[j]+nums[k]==0) { // target in this case is 0. It can be any value
-                        res={nums[i],nums[j],nums[k]};
-                        sort(res.begin(),res.end()); // sort the triple
-                        temp.insert(res);            // insert the triple in map so that duplicates are removed
+                    if(nums[i]+nums[j]+nums[k]==0) {                // target in this case is 0. It can be any value
+                         
+                        temp.insert({nums[i],nums[j],nums[k]});            // insert the triple in map so that duplicates are removed
                     }
                 }   
             }        
