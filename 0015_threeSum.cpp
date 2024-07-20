91acf520db366c3e9fb0e7a1aad56e42cd5fb65b
zainf2327 <zainchoudery786@gmail.com>
2024-07-20 15:04:12 +0500

    HashMap Method || Time Complexity: O(n2) Space Complexity: O(n)

--- 0015_threeSum.cpp
+++ 0015_threeSum.cpp
@@ -24,19 +24,17 @@ public:
         for (int i = 0; i < n - 2; i++)    {   //Fixing 1st number to tranverse the array
 
             if (nums[i] > 0)    break;      // if fixed number is +ve, stop it since leading elements would ony increase the sum and we cannot acheive 0(target).(only if target==0)
-            
-            for (int j = i + 1; j < n - 1; j++)    {   //Fixing 2nd number
+            if(i > 0 && nums[i]==nums[i-1] ) continue;      //// Continue to next iteration if 1st fixed number is same as previous to avoid dulicates. 
+           
+                for (int j = i + 1; j < n - 1; j++)    {   //Fixing 2nd number
 
-                
-                int complement = -1 * (nums[i] + nums[j]);     //complement = target - nums[i]-nums[j]
+                    if(j > i+1 && nums[j]==nums[j-1] ) continue;   // Continue to next iteration if 2nd  fixed number is same as previous to avoid dulicates. 
+                    int complement = -1 * (nums[i] + nums[j]);     //complement = target - nums[i]-nums[j]
 
-                if (hashMap.count(complement) && hashMap[complement] > j)
-                        triples.push_back({nums[i], nums[j], complement});      //If it exists in hashmap and its last occurrence index > 2nd fixed index, we found our triplet.
+                    if (hashMap.count(complement) && hashMap[complement] > j)
+                            triples.push_back({nums[i], nums[j], complement});      //If it exists in hashmap and its last occurrence index > 2nd fixed index, we found our triplet.                          
 
-                j=hashMap[nums[j]];                     //Update j to last occurence of 2nd fixed number to avoid duplicate triplets.
-            }
-
-            i=hashMap[nums[i]];                       //Update i to last occurence of 1st fixed number to avoid duplicate triplets.
+                }
         }
         return triples;
     }       
