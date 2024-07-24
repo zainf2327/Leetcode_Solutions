41f25351812a62423c82fb960324be02372e3a15
zainf2327 <zainchoudery786@gmail.com>
2024-07-24 11:22:36 +0500

    Best Solution Two pointers Time Complexity : O(n2) Space Complexity: O(n)

--- 0015_threeSum.cpp
+++ 0015_threeSum.cpp
@@ -7,27 +7,40 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
-        if (n < 3)  return {};          // Base case 1
-        sort(nums.begin(), nums.end());
-        if(nums [0]> 0 ) return {};     // Base case 2
-        unordered_set<int> s;           // HashSet for storing the elements of array.
+        sort(nums.begin(), nums.end()); // Sorted Array
+        if (nums[0] > 0)
+            return {}; // Base case
         vector<vector<int>> triples;
-
-        for (int i = 0; i < n - 1; i++) {
-            
-            if (i > 0 && nums[i] == nums[i - 1])    continue;   //avoid duplicates
-
-            for (int j = i + 1; j < n; j++) { 
-
-                int complement = -1*(nums[i] + nums[j]);
-                if (triples.empty() || nums[j] != triples.back()[2] || nums[i] != triples.back()[0]) {   // Avoid Duplicates condition
-                    if (s.count(complement))
-                        triples.push_back({nums[i], complement, nums[j]});
+        int sum = 0, start, end;
+        for (int i = 0; i < n - 2; i++) {
+            if (nums[i] > 0)
+                break;
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            start = i + 1, end = n - 1;
+            while (start < end) {
+                sum = nums[i] + nums[start] + nums[end];
+                if (sum == 0) {
+                    triples.push_back({nums[i], nums[start], nums[end]});
+                    start++;
+                    end--;
+                    while (start < end && nums[start] == nums[start - 1])  // Skipping duplicates possible
+                        start++;
+                    while (start < end && nums[end] == nums[end + 1])
+                        end--;
+                } else if (sum > 0){
+                    end--;
+                    while (start < end && nums[end] == nums[end + 1])  // Next triplet would be same, so don't need to check them.
+                        end--;
                 }
-                s.insert(nums[j]);     
+                else{
+                    start++;
+                    while (start < end && nums[start] == nums[start - 1])   // Next triplet would be same, so don't need to check them.
+                    start++;
+                }
             }
-            s.clear();   //Emptied HashSet
         }
+
         return triples;
     }
 };
