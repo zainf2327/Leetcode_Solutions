a6783bad3cf4ec44a299f99ff2e8dda858c6301e
zainf2327 <zainchoudery786@gmail.com>
2024-07-27 15:56:06 +0500

    Two pointers Approach || Time Complexity: O(n2) Space Complexity: O(1)

--- 0016_threeSumClosest.cpp
+++ 0016_threeSumClosest.cpp
@@ -1,33 +1,39 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <algorithm>
 using namespace std;
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target)  {
-        int n = nums.size(),sum=0;
-        int minDistance=INT_MAX,currentDistance,closestSum;
+        int n = nums.size();
+        if(n==3) return nums[0]+nums[1]+nums[2];
+        sort(nums.begin(),nums.end());
+        int sum=0,closestSum= nums[0]+nums[1]+nums[2];   // can also be INT_MAX/10 .
+        int start,end;
 
         for(int i =0;i<n-2;i++) {
-             for(int j =i+1;j<n-1;j++)    {
-                    for(int k=j+1;k<n;k++)    {
-                        sum=nums[i]+nums[j]+nums[k];
-                            if(sum==target) return sum;
-                            currentDistance=abs(target-sum);
-                            if(currentDistance<minDistance) {
-                                minDistance = currentDistance;
-                                closestSum= sum;
-                            }
-                    }
-            }
+            start=i+1,end=n-1;
+            while(start<end)   {
+                sum=nums[i]+nums[start]+nums[end];
+                if(sum==target) return target;
+                if(abs(target-sum)<abs(target-closestSum))  {
+                    closestSum=sum;
+                }
+                if (sum > target)  {
+                    end--;
+                    while (start < end && nums[end] == nums[end + 1])  // Next triplet would be same, so don't need to check them.
+                        end--;
+                }
+
+                else   {
+                    start++;
+                    while (start < end && nums[start] == nums[start - 1])   // Next triplet would be same, so don't need to check them.
+                    start++;
+                }             
+            }        
+            
         }
-        return closestSum;
-        
+        return closestSum;     
     }
 };
-int main()  {
-             
-             
-             
-    return 0;
-}
