c479e15dfec620f4f0c10c96666d356afb8e3ae5
zainf2327 <zainchoudery786@gmail.com>
2024-07-30 13:19:12 +0500

    Twopass HashMap apprach Time compelxity: O(n3) Space complexity : O(n)

--- 0018_fourSum.cpp
+++ 0018_fourSum.cpp
@@ -1,33 +1,37 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <set>
+#include <unordered_map>
 using namespace std;
 
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<int>temp;
-        set <vector<int>> rec;
-        int n =nums.size();
-        for(int i =0;i<n-3;i++) {
-            for(int j=i+1;j<n-2;j++)    {
-                for(int k =j+1;k<n-1;k++)   {
-                    for(int l =k+1;l<n;l++) {
-                        long long int sum= nums[i] + nums[j];
-                        sum+=nums[k];
-                        sum+=nums[l];
-                        if(sum==target) {
-                            temp={nums[i],nums[j],nums[k],nums[l]};
-                            sort(temp.begin(),temp.end());
-                            rec.insert(temp);
-                        }
+         int n = nums.size();
+        sort(nums.begin(), nums.end());
+        unordered_map<long long int, int> arrMap;
+        for (int i = 0; i < n; i++) {
+            arrMap[nums[i]] = i;
+        }
+        vector<vector< int>> res;
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+                for (int k = j + 1; k < n - 1; k++) {
+                    if (k > j + 1 && nums[k] == nums[k - 1])
+                        continue;
+                    long long int complement = (long long)target -1*( (long long)nums[i] +nums[j] + nums[k]);
+                    if (arrMap.count(complement) && arrMap[complement] > k) {
+                        res.push_back({nums[i], nums[j], nums[k], (int)complement});
                     }
                 }
             }
         }
-
-        vector<vector<int>> res (rec.begin(),rec.end());
         return res;
     }
+       
 };
