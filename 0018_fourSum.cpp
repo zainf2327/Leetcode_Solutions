c333a022b442926b87dba2db1ee6d322d455c460
zainf2327 <zainchoudery786@gmail.com>
2024-07-30 13:47:57 +0500

    Two Pointers Approach Time Complexity  : O(n3) Space Complexity : 0(1)

--- 0018_fourSum.cpp
+++ 0018_fourSum.cpp
@@ -9,27 +9,41 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
          int n = nums.size();
         sort(nums.begin(), nums.end());
-        unordered_map<long long int, int> arrMap;
-        for (int i = 0; i < n; i++) {
-            arrMap[nums[i]] = i;
-        }
+        
         vector<vector< int>> res;
-
+        int left,right;
+        long long sum;
         for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
+            if (i > 0 && nums[i] == nums[i - 1])  continue;
             for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1])
-                    continue;
-                for (int k = j + 1; k < n - 1; k++) {
-                    if (k > j + 1 && nums[k] == nums[k - 1])
-                        continue;
-                    long long int complement = (long long)target -1*( (long long)nums[i] +nums[j] + nums[k]);
-                    if (arrMap.count(complement) && arrMap[complement] > k) {
-                        res.push_back({nums[i], nums[j], nums[k], (int)complement});
-                    }
+                if (j > i + 1 && nums[j] == nums[j - 1])  continue;
+                    left =j+1;
+                    right = n-1;
+                    while(left<right)   {
+                        if(left>j+1 && nums[left]==nums[left-1])    {
+                            left++;
+                            continue;
+                        }
+                        if(right < n-1 && nums[right]==nums[right+1])  {
+                            right--;
+                            continue;
+                        }
+                        sum= nums[i];
+                        sum=sum+nums[j]+nums[left]+nums[right];
+                        if(sum==target) { 
+                            res.push_back({nums[i],nums[j],nums[left],nums[right]});
+                            left++;
+                            right--;
+                        }   
+                        else if(sum<target) {
+                            left++;
+                        }
+                        else    {
+                            right--;
+                        }
+                   }
+                
                 }
-            }
         }
         return res;
     }
