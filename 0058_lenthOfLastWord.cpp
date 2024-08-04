257a1318973cadda70f87ca30dd00226e8424b7b
zainf2327 <zainchoudery786@gmail.com>
2024-08-04 23:43:19 +0500

    Smart Approach Time Complexity: O(n) Space Complexity: O(1)

--- 0058_lenthOfLastWord.cpp
+++ 0058_lenthOfLastWord.cpp
@@ -4,16 +4,12 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int n = s.size(),count=0;
-        for(int i=n-1;i>=0;i--) {
-            if(s[i]==' ' && count==0) continue;
-            if(s[i]!=' '){
-                 count++;
-            }
-            else 
-              return count;     
+       int n = s.size()-1,count=0;
+        while(s[n]==' ') --n;
+        while(n >= 0 && s[n]!=' '){
+            count++;
+            n--;
         }
-        return count;
-        
+        return count;  
     }
 };
