cdde58a005772d90ea56d1f9488a3e09503c6629
zainf2327 <zainchoudery786@gmail.com>
2024-08-08 23:20:18 +0500

    Smart Solution Time Complexity: O(1) Space Complexity: O(1)

--- 2582_passThePillow.cpp
+++ 2582_passThePillow.cpp
@@ -2,16 +2,9 @@ class Solution {
 public:
     int passThePillow(int n, int t)  {
            
-        int N = 2*n-2 ;   // total no of dintinct passes for n friends
+        int N = 2*n-2;   // total no of dintinct passes for n friends
         int T = t % N;    // # of pass(# of seconds) 
-        if(T==0) T = N;   // for last reverse pass T=0, so, T=N;
-        if(T<n) {     // Forward pass
-            return T+1;
-        }
-        else    {       // Reverse Pass
-            return N-T+1;
-        }
-
+        return 1 + ((T<n)? T:N-T) ;    // Forward pass T+1  // Reverse Pass N-T+1     N-T+1;
 
     }
 
