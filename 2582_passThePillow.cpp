b848af5b59e56030bec991b61b00c5c2947cb1fe
zainf2327 <zainchoudery786@gmail.com>
2024-08-08 22:57:40 +0500

    Magic solution Time Complexity: O(1) Space Complexity: O(1)

--- 2582_passThePillow.cpp
+++ 2582_passThePillow.cpp
@@ -1,22 +1,19 @@
-# include <vector>
-using namespace std;
+
 class Solution {
 public:
     int passThePillow(int n, int t)  {
            
         int N = 2*n-2 ;   // total no of dintinct passes for n friends
-        int T = t % N;    // exact no of pass e.g 1th pass. 
+        int T = t % N;    // # of pass(# of seconds) 
         if(T==0) T = N;   // for last reverse pass T=0, so, T=N;
-        vector<int> arr(N,0);
-        for (int i = 0; i < N; i++) {
-            if (i < n - 1)  
-                arr[i]=i+2;   
-            else
-               arr[i]= N-i;
-            
+        if(T<n) {     // Forward pass
+            return T+1;
+        }
+        else    {       // Reverse Pass
+            return N-T+1;
         }
 
-        return arr[T-1] ;
+
     }
 
 };
