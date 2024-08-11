0dcaabdcb7065e4f2d3e333923a76b5fc9e894bd
zainf2327 <zainchoudery786@gmail.com>
2024-08-11 23:51:14 +0500

    Simple Solution Time Complexity: O(n) Space Complexity: O(1)

---
+++ 3110_scoreOfString.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int n = s.size();
+        int score = 0;
+        for (int i = 0; i < n - 1; i++) {
+            score += abs(s[i] - s[i + 1]);
+        }
+        return score;
+    }
+};
