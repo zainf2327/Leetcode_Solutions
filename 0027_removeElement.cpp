88853a4df91a31d40fabf7768cafad74e37960fd
zainf2327 <zainchoudery786@gmail.com>
2024-08-12 21:39:17 +0500

    Two pointers Approach Time Complexity: O(n) Space Complexity: O(1)

---
+++ 0027_removeElement.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        int j = 0;  
+        for(int i = 0; i < n ; i++)    {
+            if(nums[i]!= val)    {
+                nums[j]=nums[i];
+                j++;
+            }
+
+        }
+        return j;
+    }
+};
