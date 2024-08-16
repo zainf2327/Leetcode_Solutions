691a33a67ffe7131b2d924808bc4a862e626cc73
zainf2327 <zainchoudery786@gmail.com>
2024-08-16 15:42:46 +0500

    Better Approach Time Complexity; O(n) Space Complexity: O(n)

--- 0238_productExceptSelf.cpp
+++ 0238_productExceptSelf.cpp
@@ -5,13 +5,20 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans(n, 1);
+        vector<int> ans(n);
+        vector<int> prefix(n);
+        vector<int> suffix(n);
+        int pre=1,suf=1;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j)
-                    continue;
-                ans[i] *= nums[j];
-            }
+           prefix[i]=pre;
+           pre*=nums[i];
+        }
+        for(int i=n-1;i>=0;i--) {
+            suffix[i]=suf;
+            suf*=nums[i];
+        }
+        for(int i=0;i<n;i++)    {
+            ans[i]=suffix[i]*prefix[i];
         }
         return ans;
     }
