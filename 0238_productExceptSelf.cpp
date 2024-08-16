e5d1447a6df2c8704eba8faaba48a92183b2ff09
zainf2327 <zainchoudery786@gmail.com>
2024-08-16 15:31:12 +0500

    Brute Force Solution Time Complexity: O(n2) Space Complexity: O(1)

---
+++ 0238_productExceptSelf.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, 1);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j)
+                    continue;
+                ans[i] *= nums[j];
+            }
+        }
+        return ans;
+    }
+};
