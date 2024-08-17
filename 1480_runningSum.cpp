a6ec032fa2b3d47b4bf1077bbe556a711fcfa69f
zainf2327 <zainchoudery786@gmail.com>
2024-08-17 16:56:58 +0500

    Prefix Sum Approach Time Complexity: O(n) Space Complexity: O(1)

---
+++ 1480_runningSum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n =nums.size();
+        vector<int> ans(n);
+         ans[0] = nums[0];
+        for(int i=1;i<n;i++)  {
+            ans[i] = nums[i]+ans[i-1];
+        }
+        return ans;
+    }
+};
