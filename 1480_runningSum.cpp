0e18533c7a32e0e9d48f1cf77ae75990c7d2ea17
zainf2327 <zainchoudery786@gmail.com>
2024-08-17 17:03:25 +0500

    Prefix Sum (Core Pattern) Time Complexity: O(n) Space Complexity; O(1)

--- 1480_runningSum.cpp
+++ 1480_runningSum.cpp
@@ -4,12 +4,12 @@ using namespace std;
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        int n =nums.size();
-        int runningSum=0;
+        int n = nums.size();
+        int prefixSum = 0;
         vector<int> ans(n);
-        for(int i=0;i<n;i++){
-            runningSum+=nums[i];
-            ans[i]=runningSum;
+        for (int i = 0; i < n; i++) {
+            ans[i] = prefixSum + nums[i];
+            prefixSum += nums[i];
         }
         return ans;
     }
