b4dc5267ca580566f5d374a3485b185af56f0102
zainf2327 <zainchoudery786@gmail.com>
2024-08-17 16:27:21 +0500

    Efficient Solution Time Complexity: O(n) Space Complexity: O(1)

--- 1929_concatArray.cpp
+++ 1929_concatArray.cpp
@@ -5,13 +5,11 @@ class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
         int n = nums.size();
-          // General Approach if you have to concatenate k times in ans array
         vector<int> ans(2 * n);
-        for (int i = 0; i < 2; i++) {   // Number of concatenations in resultant array
-            for (int j = 0; j < n; j++) {
-                ans[j + n * i] = nums[j];
-            }
+        for (int j = 0; j < n; j++) {
+            ans[j] = ans[j + n] = nums[j];
         }
+
         return ans;
     }
 };
