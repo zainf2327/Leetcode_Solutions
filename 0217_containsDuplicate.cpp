627d86fad8fe82eee56bc469100a7000287b5078
zainf2327 <zainchoudery786@gmail.com>
2024-08-20 21:18:52 +0500

    Efficient Solution Time Complexity: O(n) Space Complexity: O(n)

--- 0217_containsDuplicate.cpp
+++ 0217_containsDuplicate.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <set>
 using namespace std;
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int n = nums.size();
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] == nums[j])
-                    return true;
+       int n = nums.size();
+        set <int> mp;
+        for (int i = 0; i < n; i++) {
+            if (mp.count(nums[i])) {
+                return true;
+            } else {
+                mp.insert(nums[i]);
             }
         }
         return false;
