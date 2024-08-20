59cff08cfa80c00d5aa2d500c09485756bbf7116
zainf2327 <zainchoudery786@gmail.com>
2024-08-20 22:40:17 +0500

    Efficient Approach Time Complexity: O(n2) Space Complexity: O(1)

--- 0118_pascalTraingle.cpp
+++ 0118_pascalTraingle.cpp
@@ -4,16 +4,13 @@ using namespace std;
  class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> ans(numRows);
+       vector<vector<int>> ans(numRows);
         for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j <= i; j++) {
-                if (j == 0 || j == i) {
-                    ans[i].push_back(1);
-                } else {
+             ans[i].resize(i+1,1);
+              for (int j = 1; j < i; j++) {
                     int num = ans[i - 1][j] + ans[i - 1][j - 1];
-                    ans[i].push_back(num);
-                }
-            }
+                    ans[i][j]=num;
+              }
         }
         return ans;
     }
