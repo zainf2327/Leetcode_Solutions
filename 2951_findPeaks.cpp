b9343f817ca73a02b7b90a089b86bc6207d69ee4
zainf2327 <zainchoudery786@gmail.com>
2024-08-23 17:33:29 +0500

    Simple Solution Time Complexlity: O(n) Space Complexity: O(1)

---
+++ 2951_findPeaks.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& mountain) {
+        vector<int> peaks;
+        int n = mountain.size();
+        for (int i = 1; i < n - 1; i++) {
+            if (mountain[i] > mountain[i - 1] &&
+                mountain[i] > mountain[i + 1]) {
+                peaks.push_back(i);
+            }
+        }
+        return peaks;
+    }
+};
