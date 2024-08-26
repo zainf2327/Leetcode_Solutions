9bbb90a48efc63cd8f55b21693002740424dad99
zainf2327 <zainchoudery786@gmail.com>
2024-08-26 21:33:40 +0500

    Brute Force Approach || Time Complexity: O(n) Space Complexity: O(n)

--- 0189_rotateArray.cpp
+++ 0189_rotateArray.cpp
@@ -4,17 +4,11 @@ using namespace std;
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
+       int n = nums.size();
         k = k % n;
-        if(k == 0) return;
-        vector<int> rotated(n);
-
-        for (int i = 0; i < n; i++) {
-            rotated[(i + k) % n] = nums[i];
-        }
-
-        for (int i = 0; i < n; i++) {
-            nums[i] = rotated[i];
-        }        
+        if (k == 0) return;
+        vector<int> temp(nums.end() - k, nums.end());
+        temp.insert(temp.end(), nums.begin(), nums.end() - k);
+        nums = temp;
     }
 };
