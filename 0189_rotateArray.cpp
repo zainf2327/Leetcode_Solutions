b81893318c01e15689a0d581396faaa3f0631229
zainf2327 <zainchoudery786@gmail.com>
2024-08-26 21:44:27 +0500

    Optimal Solution Time Complexity; O(n) Space Complexity; O(1)

--- 0189_rotateArray.cpp
+++ 0189_rotateArray.cpp
@@ -2,13 +2,23 @@
 #include <vector>
 using namespace std;
 class Solution {
+private:
+    void reverse(vector<int>& arr, int l, int r) {
+        while (l < r) {
+            swap(arr[l], arr[r]);
+            l++;
+            r--;
+        }
+    }
 public:
     void rotate(vector<int>& nums, int k) {
-       int n = nums.size();
+        int n = nums.size();
         k = k % n;
         if (k == 0) return;
-        vector<int> temp(nums.end() - k, nums.end());
-        temp.insert(temp.end(), nums.begin(), nums.end() - k);
-        nums = temp;
+        
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
     }
 };
+
