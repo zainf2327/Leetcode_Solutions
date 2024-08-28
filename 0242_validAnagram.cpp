4f049b2280b366653c7ce06c0c1e6b976d1588a4
zainf2327 <zainchoudery786@gmail.com>
2024-08-28 23:37:20 +0500

    Brute Force Approach Time Complexity: O(nlogn) Space Complexity: O(1)

---
+++ 0242_validAnagram.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+bool isAnagram(string s, string t) {
+    int n = s.size();
+        int m = t.size();
+        if (n != m)
+            return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }
+};
