22a0d4d187d0dddf2384021961065cbe0a68e54e
zainf2327 <zainchoudery786@gmail.com>
2024-08-30 22:29:04 +0500

    Better Solution Time Complexity: O(m+n) Space Complexity: O(m+n)

--- 0088_mergeSortedArray.cpp
+++ 0088_mergeSortedArray.cpp
@@ -5,10 +5,28 @@ using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i=0;i<n;i++) {
-            nums1[i+m]=nums2[i];
+       vector<int> temp(m + n);
+        int k = 0, i = 0, j = 0;
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                temp[k] = nums1[i];
+                i++;
+            } else {
+                temp[k] = nums2[j];
+                j++;
+            }
+            k++;
         }
-        sort(nums1.begin(),nums1.end());
-        
+        while (i < m) {
+            temp[k] = nums1[i];
+            i++;
+            k++;
+        }
+        while (j < n) {
+            temp[k] = nums2[j];
+            j++;
+            k++;
+        } 
+        copy(temp.begin(),temp.end(),nums1.begin());
     }
 };
