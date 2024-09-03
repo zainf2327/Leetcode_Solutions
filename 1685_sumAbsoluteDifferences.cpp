9a09a5a34301c5530265502256040d33d252bf64
zainf2327 <zainchoudery786@gmail.com>
2024-09-03 12:02:34 +0500

    Optimal Solution(Prefix Sum) Time Complexity: O(N+N) Space Complexity: O(1)

--- 1685_sumAbsoluteDifferences.cpp
+++ 1685_sumAbsoluteDifferences.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <vector>
+#include <numeric>
 using namespace std;
 class Solution {
 public:
     vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
         int n = nums.size();
+        int suffixSum = accumulate(nums.begin(), nums.end(), 0);
+        int prefixSum = 0;
         vector<int> res;
         for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = 0; j < n; j++) {
-                sum += abs(nums[i] - nums[j]);
-            }
-            res.push_back(sum);
+            suffixSum -= nums[i];
+            int leftCount = i;
+            int rightCount = n - 1 - i;
+            int leftTotal = (nums[i] * leftCount) - prefixSum;
+            int rightTotal = suffixSum - (nums[i] * rightCount);
+            res.push_back(leftTotal+rightTotal);
+            prefixSum += nums[i];
         }
         return res;
     }
