1b993c78b68276920f9d4c6b3f506bf162250b3f
zainf2327 <zainchoudery786@gmail.com>
2024-09-05 23:40:57 +0500

    Optimal Solution Time Complexity: O(n) Space Complexity: O(1)

--- 0169_majorityElement.cpp
+++ 0169_majorityElement.cpp
@@ -6,18 +6,18 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
        int n = nums.size();
-       unordered_map <int,int>count;
-       for(int i =0;i<n;i++)  {
-            count[nums[i]]++;
-       }
-       int threshold = n/2;
-       for(auto pair: count)   {
-            if(pair.second >threshold){
-                return pair.first;
+        int count = 0;
+        int candidate = 0;
+        for (int i = 0; i < n; i++) {
+            if (count == 0) {
+                candidate = nums[i];
+                count++;
+            } else if (nums[i] == candidate) {
+                count++;
+            } else {
+                count--;
             }
         }
-
-        return -1; // -1 if there is no majority element
+        return candidate;
     }
-
 };
