60d13e83e6ed3591597383a0eefc4afeb07775f8
zainf2327 <zainchoudery786@gmail.com>
2024-09-05 23:47:17 +0500

    Optimal Solution Time Complexity: O(n) Space Compelxity: O(1)

--- 0229_majorityElement(2).cpp
+++ 0229_majorityElement(2).cpp
@@ -5,25 +5,41 @@ using namespace std;
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> elementCountMap;
-        // Iterate through the input array to count element occurrences
-        for(int i = 0; i < nums.size(); i++) {
-            elementCountMap[nums[i]]++;
-        }
-        
-        vector<int> majorityElements;
-        int threshold = nums.size() / 3;
-        
-        // Iterate through the frequency map to identify majority elements
-        for(auto elementCountPair : elementCountMap) {
-            int element = elementCountPair.first;
-            int count = elementCountPair.second;  
-            // Check if the element count is greater than the threshold
-            if(count > threshold) {
-                majorityElements.push_back(element);
+       int candidate1 = 0, candidate2 = 0;
+        int count1 = 0, count2 = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (count1 == 0 && nums[i] != candidate2) {
+                candidate1 = nums[i];
+                count1++;
+            } else if (count2 == 0 && nums[i] != candidate1) {
+                candidate2 = nums[i];
+                count2++;
+            } else if (candidate1 == nums[i]) {
+                count1++;
+            } else if (candidate2 == nums[i]) {
+                count2++;
+            } else {
+                count1--;
+                count2--;
             }
         }
-        
-        return majorityElements; 
+        vector<int> ans;
+        int threshold = n / 3;
+        count1 = 0, count2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == candidate1) {
+                count1++;
+            } else if (nums[i] == candidate2) {
+                count2++;
+            }
+        }
+        if (count1 > threshold) {
+            ans.push_back(candidate1);
+        }
+        if (count2 > threshold) {
+            ans.push_back(candidate2);
+        }
+        return ans;
     }
 };
