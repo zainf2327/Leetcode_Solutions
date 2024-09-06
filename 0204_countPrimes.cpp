434d39ce6193990e411c9422871014fcb246c3a8
zainf2327 <zainchoudery786@gmail.com>
2024-09-06 22:55:34 +0500

    Brute Force Appraoch || Time Complexity: O(nlognlogn) Space Complexity:O(n)

--- 0204_countPrimes.cpp
+++ 0204_countPrimes.cpp
@@ -8,17 +8,13 @@ public:
             return 0;
         vector<bool> prime(n, true);
         prime[0] = prime[1] = false;
-        for (int i = 2; i * i < n; i += 1) {
-            if (prime[i]) {
-                for (int j = i * i; j < n; j += i) {
-                    prime[j] = false;
-                }
-            }
-        }
         int count = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 2; i < n; i += 1) {
             if (prime[i]) {
                 count++;
+                for (int j = 2 * i; j < n; j += i) {
+                    prime[j] = false;
+                }
             }
         }
         return count;
