0d8f780c2c55c21208e3de0e2cbaec68131cd43d
zainf2327 <zainchoudery786@gmail.com>
2024-09-06 23:04:26 +0500

    Better Solution Time Complexity : O(nlognlogn +n) Space Complexity: O(n)

--- 0204_countPrimes.cpp
+++ 0204_countPrimes.cpp
@@ -4,19 +4,29 @@ using namespace std;
 class Solution {
 public:
     int countPrimes(int n) {
-        if (n <= 2)
+        if (n <= 2) {
             return 0;
-        vector<bool> prime(n, true);
-        prime[0] = prime[1] = false;
-        int count = 0;
-        for (int i = 2; i < n; i += 1) {
+        }
+        vector<bool> prime(n, false);
+        for (int i = 3; i < n; i += 2) {
+            prime[i] = true;
+        }
+        prime[2] = true;
+        for (int i = 3; i * i < n; i += 2) {
             if (prime[i]) {
-                count++;
-                for (int j = 2 * i; j < n; j += i) {
+                for (int j = i * i; j < n; j += 2 * i) {
                     prime[j] = false;
                 }
             }
         }
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (prime[i]) {
+                count++;
+            }
+        }
+
         return count;
     }
 };
+ 
