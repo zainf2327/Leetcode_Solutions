f7184074cc82ec3ea0e2d0ad4042e81a24f191d9
zainf2327 <zainchoudery786@gmail.com>
2024-09-07 23:55:05 +0500

    Simple Solution

--- 0535_tinyUrl.cpp
+++ 0535_tinyUrl.cpp
@@ -2,28 +2,10 @@
 #include <vector>
 #include<unordered_map>
 #include<string>
-#include <random>
 using namespace std;
-
 class Solution {
 private:
-    string getRandomAlphaNumericString(int length) {
-        const string alphanum = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                                "abcdefghijklmnopqrstuvwxyz"
-                                "0123456789";
-        random_device rd;        // Seed for the random number engine
-        mt19937 generator(rd()); // Mersenne Twister random number generator
-        uniform_int_distribution<> distr(0,alphanum.size() - 1); // Unifrom Distribution
-        string randomStr;
-        for (int i = 0; i < length; ++i) {
-            randomStr +=
-                alphanum[distr(generator)]; // Append a random character
-        }
-
-        return randomStr;
-    }
     const string NAME = "https://decentraldevelopers.com/";
-    const int LENGTH = 6;
     unordered_map<string, string> encodeMap;
     unordered_map<string, string> decodeMap;
 
@@ -33,10 +15,7 @@ public:
         if (encodeMap.count(longUrl)) {
             return NAME + encodeMap[longUrl];
         }
-        string shortUrl;
-        do {
-            shortUrl = getRandomAlphaNumericString(LENGTH);
-        } while (decodeMap.count(shortUrl));
+        string shortUrl = to_string(encodeMap.size());
         encodeMap[longUrl] = shortUrl;
         decodeMap[shortUrl] = longUrl;
         return NAME + encodeMap[longUrl];
@@ -44,11 +23,7 @@ public:
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        shortUrl = shortUrl.substr(NAME.size(), LENGTH);
+        shortUrl = shortUrl.substr(NAME.size());
         return decodeMap[shortUrl];
     }
 };
-
-// Your Solution object will be instantiated and called as such:
-// Solution solution;
-// solution.decode(solution.encode(url));
