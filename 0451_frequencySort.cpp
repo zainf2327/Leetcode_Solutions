e1c97670ef7e2f152bef07dac6a03d8f72bd0f24
zainf2327 <zainchoudery786@gmail.com>
2024-09-13 00:27:48 +0500

    Optimal Solution Time Complexity: O(n) Space Complexity: O(n)

--- 0451_frequencySort.cpp
+++ 0451_frequencySort.cpp
@@ -5,19 +5,26 @@
 using namespace std;
 class Solution {
 private:
-    unordered_map<char, int> count;
+    static const int SIZE = 128; // 127 ASCII characters are there
+    int count[SIZE] = {0};
 public:
     string frequencySort(string s) {
+        int n = s.size();
         for (char c : s) {
             count[c]++;
         }
-        auto comparator = [this](char a, char b) -> bool {
-            if (count[a] != count[b])
-                return count[a] > count[b];
-            return a < b;
-        };
-        sort(s.begin(), s.end(), comparator);
-
-        return s;
+        vector<string> bucket(n + 1);
+        for (int i = 0; i < SIZE; i++) {
+            if (count[i] == 0)
+                continue;
+            bucket[count[i]].push_back((char)i);
+        }
+        string ans = "";
+        for (int i = n; i >= 0; i--) {
+            for (char c : bucket[i]) {
+                ans.append(i, c); // Append 'i' occurrences of character 'c'
+            }
+        }
+        return ans;
     }
 };
