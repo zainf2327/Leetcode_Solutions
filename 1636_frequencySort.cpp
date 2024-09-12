142df5e3155d6ec70ada7e66c5b88d0ee022c11f
zainf2327 <zainchoudery786@gmail.com>
2024-09-13 01:35:14 +0500

    Bucket Sort Time Complexity: O(n+k) Space Complexity: O(n+k)

--- 1636_frequencySort.cpp
+++ 1636_frequencySort.cpp
@@ -6,16 +6,30 @@ using namespace std;
 class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
-        unordered_map<int, int> count;
+        int n = nums.size();
+        int Max = nums[0], Min = nums[0];
         for (int num : nums) {
-            count[num]++;
+            Min = min(Min, num);
+            Max = max(Max, num);
         }
-        auto comparator = [&count](int a, int b) {
-            if (count[a] != count[b])
-                return count[a] < count[b];
-            return a > b;
-        };
-        sort(nums.begin(), nums.end(), comparator);
-        return nums;
+        const int SIZE = (Max - Min) + 1;
+        vector<int> count(SIZE, 0);
+        for (int num : nums) {
+            count[num - Min]++;
+        }
+        vector<vector<int>> freBucket(n + 1);
+        for (int i = SIZE - 1; i >= 0; i--) {
+            if (count[i] == 0)
+                continue;
+            freBucket[count[i]].push_back(i + Min);
+        }
+        vector<int> ans;
+        ans.reserve(n);
+        for (int i = 1; i <= n; i++) {
+            for (int x : freBucket[i]) {
+                ans.insert(ans.end(), i, x);
+            }
+        }
+        return ans;
     }
 };
