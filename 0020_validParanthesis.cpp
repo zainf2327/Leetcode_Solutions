ecde10319fa4b83a7704eb015517dcfd795c2020
zainf2327 <zainchoudery786@gmail.com>
2024-09-18 23:52:07 +0500

    Readable Solution Time Complexity: O(n) Space Complexity: O(n)

--- 0020_validParanthesis.cpp
+++ 0020_validParanthesis.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <stack>
+#include <unordered_map>
 using namespace std;
 class Solution {
 public:
     bool isValid(string s) {
         stack<char> stc;
+        unordered_map<char, char> closeToOpen = {
+            {'}', '{'}, {']', '['}, {')', '('}};
         for (char c : s) {
-            if (c == '{' || c == '[' || c == '(') {
-                stc.push(c);
-            } else {
-                if (stc.empty() || c == ']' && stc.top() != '[' ||
-                    c == '}' && stc.top() != '{' ||
-                    c == ')' && stc.top() != '(') {
+            if (closeToOpen.count(c)) {
+                if (stc.empty() || closeToOpen[c] != stc.top()) {
                     return false;
                 }
                 stc.pop();
+
+            } else {
+                stc.push(c);
             }
         }
         return stc.empty();
