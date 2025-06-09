bd9bde25157125898869782de127f750f4efe15d
Muhammad Zain <164072957+zainf2327@users.noreply.github.com>
2025-06-09 16:38:56 +0500

    Better Time Complexity: O(N+Nlogk) Space Complexity: O(n+k)

--- 0347_topKFrequentElements.cpp
+++ 0347_topKFrequentElements.cpp
@@ -1,29 +1,30 @@
-#include <vector>
 #include <unordered_map>
-#include <algorithm>
-
+#include <vector>
+#include <queue>
+#include <functional>  // if you need std::greater or std::less
 using namespace std;
 
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> freq;
-
+        unordered_map<int, int> count;
         for (int num : nums) {
-            freq[num]++;
-        }
-        vector<pair<int, int>> freqVec(freq.begin(), freq.end());
-
-        auto compare = [](const pair<int,int>& a, const pair<int,int>& b) {
-            return a.second > b.second;
-        };
-
-        sort(freqVec.begin(), freqVec.end(), compare);
-        vector<int> result;
-        for (int i = 0; i < k; ++i) {
-            result.push_back(freqVec[i].first);
+            count[num]++;
         }
 
-        return result;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap;
+        for (auto& entry : count) {
+            heap.push({entry.second, entry.first});
+            if (heap.size() > k) {
+                heap.pop();
+            }
+        }
+
+        vector<int> res;
+        for (int i = 0; i < k; i++) {
+            res.push_back(heap.top().second);
+            heap.pop();
+        }
+        return res;
     }
 };
