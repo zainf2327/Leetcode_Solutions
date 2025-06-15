6a638d8d34ed03794099221c674f0d26172bffa7
Muhammad Zain <164072957+zainf2327@users.noreply.github.com>
2025-06-15 15:23:03 +0500

    Better Appraoch Time Complexity: O(2n)=O(n) Space Complexity: O(n)

--- 0042_trappingRainWater.cpp
+++ 0042_trappingRainWater.cpp
@@ -4,17 +4,17 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        vector<int> leftMax(n);
+        int leftMax = height[0];
         vector<int> rightMax(n);
-        leftMax[0] = height[0];
         rightMax[n - 1] = height[n - 1];
-        for (int l = 1, r = n - 2; l < n; l++, r--) {
-            leftMax[l] = max(leftMax[l - 1], height[l]);
+        for (int r = n - 2; r >= 0; r--) {
             rightMax[r] = max(rightMax[r + 1], height[r]);
         }
         int res = 0;
+
         for (int i = 0; i < n; i++) {
-            res += min(leftMax[i], rightMax[i]) - height[i];
+            leftMax = max(leftMax, height[i]);
+            res += min(leftMax, rightMax[i]) - height[i];
         }
         return res;
     }
