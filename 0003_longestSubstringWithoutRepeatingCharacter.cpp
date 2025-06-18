70eeb6f160af94c1ffa22a095e79f484e5a27a36
Muhammad Zain <164072957+zainf2327@users.noreply.github.com>
2025-06-18 14:49:39 +0500

    Better Approach Time Complezity: O(2n) Space Complexity: O(256)

--- 0003_longestSubstringWithoutRepeatingCharacter.cpp
+++ 0003_longestSubstringWithoutRepeatingCharacter.cpp
@@ -5,15 +5,20 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n = s.size(),maxLen=0;
-        for (int l = 0; l < n; l++) {
-            bool seen[256]={0};
-            for (int r = l; r < n; r++) {
-                if(seen[s[r]])   break;
-                maxLen=max(r-l+1,maxLen);
-                seen[s[r]]=true;
+        int n = s.size(), maxLen = 0;
+        int l = 0, r = 0;
+        bool Set[256] = {false};
+
+        while (r < n) {
+            while (Set[s[r]]) {
+                Set[s[l]] = false;
+                l++;
             }
+            Set[s[r]] = true;  
+            maxLen = max(maxLen, r - l + 1);
+            r++;
         }
+
         return maxLen;
     }
 };
