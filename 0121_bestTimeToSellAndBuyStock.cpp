9f96b143fcb4dfc5b1fb7d365d6d0e332e4852ed
Muhammad Zain <164072957+zainf2327@users.noreply.github.com>
2025-06-18 14:19:19 +0500

    Optimal Solution Time Complexity:O(n) Space Complexity: O(1)

--- 0121_bestTimeToSellAndBuyStock.cpp
+++ 0121_bestTimeToSellAndBuyStock.cpp
@@ -3,14 +3,17 @@ using namespace std ;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int maxPro=0,n=prices.size();
-        for(int l=0;l<n;l++)    {
-            for(int r=l+1;r<n;r++)  {
-                int profit=prices[r]-prices[l];
-                maxPro=max(maxPro,profit);
-            }
+        int maxPro = 0, n = prices.size();
+        int l = 0, r = 0;
+        while (r < n) {
+            if (prices[r] >= prices[l]) {
+                int profit = prices[r] - prices[l];
+                maxPro = max(maxPro, profit);
+            } else
+                l = r;
+
+            r++;
         }
         return maxPro;
-        
     }
 };
