bf918108332ebcf735f6041fe0eb2ce3172ed55a
Muhammad Zain <164072957+zainf2327@users.noreply.github.com>
2025-07-01 11:08:51 +0500

    Optimal Solution Time Complexity: O(4n) Space Complexity:O(1)

--- 0930_binarySubarrayswithSum.cpp
+++ 0930_binarySubarrayswithSum.cpp
@@ -1,21 +1,29 @@
 # include <vector>
 using namespace std;
-
 class Solution {
+private:
+    int numSubarraysWithSumAtmost(vector<int>& nums, int target) {
+        if (target < 0)
+            return 0;
+        int count = 0;
+        int l = 0;
+        int sum = 0;
+        for (int r = 0; r < nums.size(); r++) {
+            sum = sum + nums[r];
+            while (sum > target) {
+                sum = sum - nums[l];
+                l++;
+            }
+
+            count = count + (r - l + 1);
+        }
+        return count;
+    }
+
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
 
-        int count = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            int sum = 0;
-            for (int j = i; j < nums.size(); j++) {
-                sum = sum + nums[j];
-                if (sum > goal)
-                    break;
-                else if (sum == goal)
-                    count++;
-            }
-        }
-        return count;
+        return numSubarraysWithSumAtmost(nums, goal) -
+               numSubarraysWithSumAtmost(nums, goal - 1);
     }
 };
