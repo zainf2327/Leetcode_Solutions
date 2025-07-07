9a668af1438a9d9d409011c026cddda5e04d42fb
Muhammad Zain <164072957+zainf2327@users.noreply.github.com>
2025-07-07 14:59:43 +0500

    Modified Binary Search Time Complexity:O(log n) in average case O(n) in worst case Space Complexity:O(1)

---
+++ 0154_findMinInRotatedSortedArray(II).cpp
@@ -0,0 +1,28 @@
+# include <vector> 
+# include <climits> 
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int ans = INT_MAX;
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int m = (l + r) / 2;
+
+            if (nums[l] == nums[m] && nums[m] == nums[r]) {
+                ans = min(ans, nums[l]);
+                l++;
+                r--;
+            } else if (nums[l] <= nums[m]) {
+                ans = min(ans, nums[l]);
+                l = m + 1;
+
+            } else {
+                ans = min(ans, nums[m]);
+                r = m - 1;
+            }
+        }
+        return ans;
+    }
+};
