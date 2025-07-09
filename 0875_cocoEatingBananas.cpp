a61525a3c8c9d45c7e81365c8491785f2772e237
Muhammad Zain <164072957+zainf2327@users.noreply.github.com>
2025-07-09 12:14:54 +0500

    Binary search Time Complexity:O(log n) Space Complexity:O(1)

--- 0875_cocoEatingBananas.cpp
+++ 0875_cocoEatingBananas.cpp
@@ -1,5 +1,6 @@
 #include <vector>
-#include <climits>
+#include <climits> 
+#include <cmath> 
 using namespace std;
 class Solution {
 private:
@@ -13,7 +14,10 @@ private:
     bool isValid(vector<int>& piles, int eatingSpeed, int hours) {
         int requiredHours = 0;
         for (int pile : piles) {
-             requiredHours += ceil((float)pile / eatingSpeed);;
+             requiredHours += ceil((float)pile / eatingSpeed);
+            // requiredHours += (pile + eatingSpeed - 1) / eatingSpeed;
+            // ceil((double)a/b) ===== (a+b-1)/b   whera a and b are both positive integers only.
+
             if (requiredHours > hours)
                 return false;
         }
@@ -22,14 +26,21 @@ private:
 
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int n = piles.size();
+         int n = piles.size();
         if (n > h)
             return -1;
+        int l = 1;
         int r = maxPiles(piles);
-        for (int l = 1; l <= r; l++) {
-            if (isValid(piles, l, h))
-                return l;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (isValid(piles, m, h)) {
+                r = m - 1;
+            }
+
+            else
+                l = m + 1;
         }
-        return -1;
+        return l;
     }
 };
+
