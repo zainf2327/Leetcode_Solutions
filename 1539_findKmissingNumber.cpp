7841f552a3a4294d86b9a7d5d4bd3e46e1a48507
Muhammad Zain <164072957+zainf2327@users.noreply.github.com>
2025-07-13 14:51:29 +0500

    Binary Search

--- 1539_findKmissingNumber.cpp
+++ 1539_findKmissingNumber.cpp
@@ -4,20 +4,16 @@ using namespace std;
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        int n = arr.size();
-        int i = 0;
-        for (i = 0; i < n; i++) {
-            if ((arr[i] - i - 1) >= k) {
-                break;
+        int l = 0, h = arr.size() - 1;
+        while (l <= h) {
+            int m = (l + h) / 2;
+            int missing = arr[m] - (m + 1);
+            if (missing < k) {
+                l = m + 1;
+            } else {
+                h = m - 1;
             }
         }
-
-        if (i == 0)
-            return k;
-        i--;
-
-        int pre = arr[i];
-        int more = k - (arr[i] - (i + 1));
-        return pre + more; // k+i+1;
+        return k + h + 1; // k+l
     }
 };
