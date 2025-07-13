7a2498e3dcee3ea9e906edb2f0983864e0f5df76
Muhammad Zain <164072957+zainf2327@users.noreply.github.com>
2025-07-13 12:36:56 +0500

    Binary Search

--- 1011_capacityToShipPackagesWithinDDays.cpp
+++ 1011_capacityToShipPackagesWithinDDays.cpp
@@ -23,13 +23,16 @@ bool isValid(vector<int>& weights, int days,int capacity)  {
 public:
 
     int shipWithinDays(vector<int>& weights, int days) {
-        int l = *max_element(weights.begin(), weights.end());
+         int l = *max_element(weights.begin(), weights.end());
         int r = accumulate(weights.begin(), weights.end(), 0);
-        for (int i = l; i <= r; i++) {
-            if (isValid(weights, days, i)) {
-                return i;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (isValid(weights, days, m)) {
+                r = m - 1;
+            } else {
+                l = m + 1;
             }
         }
-        return -1;
+        return l;
     }
 };
