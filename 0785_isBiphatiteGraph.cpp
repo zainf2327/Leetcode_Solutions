95c18992463731559bdba9f9d74a128c5256e1e0
Muhammmad Zain <zainchoudery786@gmail.com>
2025-07-29 23:49:24 -0700

    dfs

---
+++ 0785_isBiphatiteGraph.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+using namespace std;
+class Solution {
+    bool dfs(int node, vector<vector<int>> & graph, vector<int>& color) {
+        for (int nei : graph[node]) {
+            if (color[nei] == -1) {
+                color[nei] = !color[node];
+                if (!dfs(nei, graph, color))
+                    return false;
+            } else {
+                if (color[node] == color[nei])
+                    return false;
+            }
+        }
+        return true;
+    }
+
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int v = graph.size();
+        vector<int> color(v,-1) ; // use it as visited array and as well as array to hold the color of each node 0 meean  red and 1 means black
+
+        for (int i = 0; i < v; i++) {
+            if (color[i] == -1) {
+                color[i] = 0;
+                if (!dfs(i, graph, color))
+                    return false;
+            }
+        }
+        return true;
+    }
+};
+
+// O(V+V+E)
+// O(V+V)
