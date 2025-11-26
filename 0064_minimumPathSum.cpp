2090b049883d7063ec2f86499324d82b51f5dd0e
Muhammmad Zain <zainchoudery786@gmail.com>
2025-11-26 18:49:56 +0500

    Tabulaiton

---
+++ 0064_minimumPathSum.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<int> dp(n, 0);
+        dp[0] = grid[0][0];
+        for (int j = 1; j < n; j++) {
+            dp[j] = dp[j - 1] + grid[0][j];
+        }
+
+        for (int i = 1; i < m; i++) {
+            dp[0] = dp[0] + grid[i][0];
+            for (int j = 1; j < n; j++) {
+                dp[j] = grid[i][j] + min(dp[j], dp[j - 1]);
+            }
+        }
+        return dp[n - 1];
+    }
+};
