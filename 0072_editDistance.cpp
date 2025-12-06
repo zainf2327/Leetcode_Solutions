bfed271e6a8dd0ae3c55972929560a8ccb678395
Muhammmad Zain <zainchoudery786@gmail.com>
2025-12-06 08:45:30 +0500

    Memoization

--- 0072_editDistance.cpp
+++ 0072_editDistance.cpp
@@ -2,20 +2,27 @@
 #include <vector>
 using namespace std;
 class Solution {
-    int f(string& s, string& t, int i, int j) {
+    int f(string& s, string& t, int i, int j,vector<vector<int>>&dp) {
         if (i == 0)
             return j;
         if (j == 0)
             return i;
+        if(dp[i][j]!=-1) return dp[i][j];
         if (s[i - 1] == t[j - 1])
-            return f(s, t, i - 1, j - 1);
+            return dp[i][j]= f(s, t, i - 1, j - 1,dp);
         else
-            return 1 + min(f(s, t, i, j - 1),                           // insert,delete,replace
-                           min(f(s, t, i - 1, j), f(s, t, i - 1, j - 1)));
+            return dp[i][j]= 1 + min(f(s, t, i, j - 1,dp),
+                           min(f(s, t, i - 1, j,dp), f(s, t, i - 1, j - 1,dp)));
     }
 
 public:
     int minDistance(string word1, string word2) {
-        return f(word1, word2, word1.size(), word2.size());   //O(3^(n+m))
+        int n=word1.size();
+        int m=word2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return f(word1, word2,n ,m,dp);
     }
 };
+
+// Memoization
+//O(n*m)
