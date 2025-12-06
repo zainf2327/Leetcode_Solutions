b1dab65309c6151fca24dfb304f0d73308683429
Muhammmad Zain <zainchoudery786@gmail.com>
2025-12-06 13:45:58 +0500

    Tabualation

--- 0583_deleteOperationsToMakeStrings.cpp
+++ 0583_deleteOperationsToMakeStrings.cpp
@@ -2,27 +2,28 @@
 #include <string>
 using namespace std;
 class Solution {
-int f(int i,int j,string& s,string& t,vector<vector<int>>& dp)  {
-    if(i==0 && j==0) return 0;
-    if(i==0 &&j>0) return j;
-    if(j==0 && i>0) return i;
-    if(dp[i][j]!=-1) return dp[i][j];
-    if(s[i-1]==t[j-1])  {
-        return dp[i][j]=f(i-1,j-1,s,t,dp);
-    }
-    else {
-        return dp[i][j]=1+min(f(i,j-1,s,t,dp),f(i-1,j,s,t,dp));
-    }
-}
+
 public:
     int minDistance(string word1, string word2) {
-        int n=word1.size();
-        int m=word2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return f(n,m,word1,word2,dp);
-        
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        dp[0][0] = 0;
+        for (int j = 1; j <= m; j++)
+            dp[0][j] = j;
+        for (int i = 1; i <= n; i++)
+            dp[i][0] = i;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else
+                    dp[i][j] = 1 + min(dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
+        return dp[n][m];
     }
 };
-//Memoization
+//Tabualtion
 // TC: O(m*n)
-//SC: O(m*n)+O(m+n)
+//SC: O(m*n))
