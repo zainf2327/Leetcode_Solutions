90c66bd01268a04303427a45e2526309ad391e15
Muhammmad Zain <zainchoudery786@gmail.com>
2025-12-06 09:52:27 +0500

    Space Optmization

--- 0072_editDistance.cpp
+++ 0072_editDistance.cpp
@@ -6,22 +6,23 @@ public:
     int minDistance(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-        dp[0][0] = 0;
+        vector<int> pre(m+1,0);
+        pre[0] = 0;
         for (int j = 1; j <= m; j++)
-            dp[0][j] = j;
-        for (int i = 1; i <= n; i++)
-            dp[i][0] = i;
+            pre[j] = j;
         for (int i = 1; i <= n; i++) {
+            vector<int>curr(m+1,0);
+            curr[0]=i;
             for (int j = 1; j <= m; j++) {
                 if (s[i - 1] == t[j - 1])
-                    dp[i][j] = dp[i - 1][j - 1];
+                    curr[j] = pre[j - 1];
                 else
-                    dp[i][j] = 1 + min(dp[i][j - 1],min(dp[i - 1][j], dp[i - 1][j - 1]));
+                    curr[j] = 1 + min(curr[j - 1],min(pre[j], pre[j - 1]));
             }
+            pre=curr;
         }
-        return dp[n][m];
+        return pre[m];
     }
 };
-
-// Tabualtion O(n*m)+ NO Stack Space
+//Space Optimization
+// O(n*m) for time and O(m) for space
