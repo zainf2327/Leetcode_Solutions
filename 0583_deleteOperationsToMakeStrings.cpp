eed5da4433cc2fc03e5d22df21039def341b0c7c
Muhammmad Zain <zainchoudery786@gmail.com>
2025-12-06 13:51:33 +0500

    Dp

--- 0583_deleteOperationsToMakeStrings.cpp
+++ 0583_deleteOperationsToMakeStrings.cpp
@@ -7,23 +7,23 @@ public:
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-        dp[0][0] = 0;
+        vector<int> pre(m + 1, 0), curr(m + 1, 0);
+        pre[0] = 0;
         for (int j = 1; j <= m; j++)
-            dp[0][j] = j;
-        for (int i = 1; i <= n; i++)
-            dp[i][0] = i;
+            pre[j] = j;
         for (int i = 1; i <= n; i++) {
+            curr[0] = i;
             for (int j = 1; j <= m; j++) {
                 if (word1[i - 1] == word2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1];
+                    curr[j] = pre[j - 1];
                 } else
-                    dp[i][j] = 1 + min(dp[i][j - 1], dp[i - 1][j]);
+                    curr[j] = 1 + min(curr[j - 1], pre[j]);
             }
+            pre = curr;
         }
-        return dp[n][m];
+        return pre[m];
     }
 };
-//Tabualtion
+//Space Optimization
 // TC: O(m*n)
-//SC: O(m*n))
+//SC: O(n))
