9f1ae0992ac930079479262d924a4cf3b1a63fb1
Muhammmad Zain <zainchoudery786@gmail.com>
2025-12-06 12:33:06 +0500

    Memoization

--- 1312_minInsertionTOMakeStringPalindrome.cpp
+++ 1312_minInsertionTOMakeStringPalindrome.cpp
@@ -1,16 +1,20 @@
 #include <string>
+#include<vector>
 using namespace std;
 class Solution {
-int minSteps(int i,int j,string& s)    {
+int f(int i,int j,string& s,vector<vector<int>>&dp)    {
     if(i>=j) return 0;
+    if(dp[i][j]!=-1) return dp[i][j];
     if(s[i]==s[j])
-        return minSteps(i+1,j-1,s);
+        return dp[i][j]= f(i+1,j-1,s,dp);
     else 
-        return 1+min(minSteps(i+1,j,s),minSteps(i,j-1,s));
+        return dp[i][j]=1+min(f(i+1,j,s,dp),f(i,j-1,s,dp));
 }
 public:
     int minInsertions(string s) {
-        return  minSteps(0,s.size()-1,s);  
+        int n=s.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return  f(0,s.size()-1,s,dp);  
     }
 };
 
