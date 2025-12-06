4e20bab3f9a28931f9b500c880e75593beb2adad
Muhammmad Zain <zainchoudery786@gmail.com>
2025-12-06 12:51:01 +0500

    Tabulation

--- 1312_minInsertionTOMakeStringPalindrome.cpp
+++ 1312_minInsertionTOMakeStringPalindrome.cpp
@@ -13,9 +13,14 @@ int f(int i,int j,string& s,vector<vector<int>>&dp)    {
 public:
     int minInsertions(string s) {
         int n=s.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return  f(0,s.size()-1,s,dp);  
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for (int l=2;l<=n;l++)  {
+            for(int i=0;i<=n-l;i++)    {
+                int j=i+l-1;
+                if(s[i]==s[j])  dp[i][j]= dp[i+1][j-1];
+                else dp[i][j]=1+min(dp[i+1][j],dp[i][j-1]);
+            }
+        } 
+        return dp[0][n-1];
     }
 };
-
-// O(2^n) +O(n)
