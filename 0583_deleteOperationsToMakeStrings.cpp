49c79816955220af65ce988fee04a90250501e36
Muhammmad Zain <zainchoudery786@gmail.com>
2025-12-06 13:40:04 +0500

    Memoization

--- 0583_deleteOperationsToMakeStrings.cpp
+++ 0583_deleteOperationsToMakeStrings.cpp
@@ -2,25 +2,27 @@
 #include <string>
 using namespace std;
 class Solution {
-int f(int i,int j,string& s,string& t)  {
+int f(int i,int j,string& s,string& t,vector<vector<int>>& dp)  {
     if(i==0 && j==0) return 0;
     if(i==0 &&j>0) return j;
     if(j==0 && i>0) return i;
+    if(dp[i][j]!=-1) return dp[i][j];
     if(s[i-1]==t[j-1])  {
-        return f(i-1,j-1,s,t);
+        return dp[i][j]=f(i-1,j-1,s,t,dp);
     }
     else {
-        return 1+min(f(i,j-1,s,t),f(i-1,j,s,t));
+        return dp[i][j]=1+min(f(i,j-1,s,t,dp),f(i-1,j,s,t,dp));
     }
 }
 public:
     int minDistance(string word1, string word2) {
         int n=word1.size();
         int m=word2.size();
-        return f(n,m,word1,word2);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return f(n,m,word1,word2,dp);
         
     }
 };
-
-// TC: O(2^(m+n))
-//SC: O(m+n)
+//Memoization
+// TC: O(m*n)
+//SC: O(m*n)+O(m+n)
