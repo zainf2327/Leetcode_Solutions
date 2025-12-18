9983525fb2d8c67eb9b5a64f76539462aecc274c
Muhammmad Zain <zainchoudery786@gmail.com>
2025-12-18 11:10:54 +0500

    Brute force solution

--- 0121_bestTimeToSellAndBuyStock.cpp
+++ 0121_bestTimeToSellAndBuyStock.cpp
@@ -1,19 +1,26 @@
 #include <vector>
 using namespace std ;
 class Solution {
+int f(int i,int tranNum,int n,vector<int>&prices)    {
+    if(i>=n || tranNum>=2) return 0;
+    if(tranNum==0)  {
+        int buy=-prices[i]+f(i+1,1,n,prices);
+        int notBuy=f(i+1,0,n,prices);
+        return max(buy,notBuy);
+    }
+    
+        int sell=prices[i]+f(i+1,2,n,prices);
+        int notSell=f(i+1,1,n,prices);
+        return max(sell,notSell);
+
+}
 public:
     int maxProfit(vector<int>& prices) {
-        int maxPro = 0, n = prices.size();
-        int l = 0, r = 0;
-        while (r < n) {
-            if (prices[r] >= prices[l]) {
-                int profit = prices[r] - prices[l];
-                maxPro = max(maxPro, profit);
-            } else
-                l = r;
-
-            r++;
-        }
-        return maxPro;
+        int n=prices.size();
+        return f(0,0,n,prices);
+        
     }
 };
+
+// recursive Solution TC: 2^n
+//SC: O(n) call stack +O(2^n)
