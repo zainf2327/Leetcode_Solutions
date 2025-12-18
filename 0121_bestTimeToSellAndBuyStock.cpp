2389693fc00b161e35a87b5ee56efca314562a74
Muhammmad Zain <zainchoudery786@gmail.com>
2025-12-18 11:16:23 +0500

    Better

--- 0121_bestTimeToSellAndBuyStock.cpp
+++ 0121_bestTimeToSellAndBuyStock.cpp
@@ -1,26 +1,31 @@
 #include <vector>
 using namespace std ;
 class Solution {
-int f(int i,int tranNum,int n,vector<int>&prices)    {
-    if(i>=n || tranNum>=2) return 0;
-    if(tranNum==0)  {
-        int buy=-prices[i]+f(i+1,1,n,prices);
-        int notBuy=f(i+1,0,n,prices);
-        return max(buy,notBuy);
-    }
-    
-        int sell=prices[i]+f(i+1,2,n,prices);
-        int notSell=f(i+1,1,n,prices);
-        return max(sell,notSell);
+    int f(int i, int tranNum, int n, vector<int>& prices,
+          vector<vector<int>>& dp) {
+        if (i >= n || tranNum >= 2)
+            return 0;
+        if (dp[i][tranNum] != -1)
+            return dp[i][tranNum];
+        if (tranNum == 0) {
+            int buy = -prices[i] + f(i + 1, 1, n, prices, dp);
+            int notBuy = f(i + 1, 0, n, prices, dp);
+            return dp[i][tranNum] = max(buy, notBuy);
+        }
 
-}
+        int sell = prices[i] + f(i + 1, 2, n, prices, dp);
+        int notSell = f(i + 1, 1, n, prices, dp);
+        return dp[i][tranNum] = max(sell, notSell);
+    }
+
 public:
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        return f(0,0,n,prices);
-        
+        int n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        return f(0, 0, n, prices, dp);
     }
 };
 
-// recursive Solution TC: 2^n
-//SC: O(n) call stack +O(2^n)
+// Memoization Solution 
+// TC: n*2
+//SC: O(n)(call stack)  +  O(n*2)
