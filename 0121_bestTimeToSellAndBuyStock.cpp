17841b6e9a4ff746c35e8b82ad8b16a1f3406127
Muhammmad Zain <zainchoudery786@gmail.com>
2025-12-18 11:26:52 +0500

    Efficient Solution

--- 0121_bestTimeToSellAndBuyStock.cpp
+++ 0121_bestTimeToSellAndBuyStock.cpp
@@ -21,11 +21,15 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return f(0, 0, n, prices, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(2 + 1, 0));
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][0] = max(dp[i + 1][0], -prices[i] + dp[i + 1][1]);
+            dp[i][1] = max(dp[i + 1][1], prices[i] + dp[i + 1][2]);
+        }
+        return dp[0][0];
     }
 };
 
-// Memoization Solution 
+// Tabulation Solution 
 // TC: n*2
-//SC: O(n)(call stack)  +  O(n*2)
+//SC: O(n*2)
