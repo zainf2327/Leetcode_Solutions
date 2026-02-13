be201586cd4e4968054dbe0e5eb9abd61115f5df
Muhammmad Zain <zainchoudery786@gmail.com>
2026-02-13 15:33:05 +0500

    added

---
+++ 02095_deleteMiddleNodeOfLinkedList.cpp
@@ -0,0 +1,28 @@
+ struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if (!head || !head->next) {
+            return nullptr;
+        }
+        ListNode* slow=head,*fast=head->next->next;
+        while(fast && fast->next) {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* temp=slow->next;
+        slow->next=slow->next->next;
+        delete temp;
+        return head;
+
+
+        
+    }
+};
