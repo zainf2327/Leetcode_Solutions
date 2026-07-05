3561bd450c5671208d640b552363e74da0297955
Muhammad Zain <zainchoudery786@gmail.com>
2026-07-05 16:21:03 +0500

    solved balanced bt

---
+++ 0110_balancedBinaryTree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+    int dfs(TreeNode* root) {
+        if (!root)
+            return 0;
+        int lh = dfs(root->left);
+        int rh = dfs(root->right);
+        if (lh == -1 || rh == -1)
+            return -1;
+        if (abs(lh - rh) > 1)
+            return -1;
+        return 1 + max(lh, rh);
+    }
+
+public:
+    bool isBalanced(TreeNode* root) { return dfs(root) != -1; }
+};
